5e61c0d8c388548e8bf00988a2f68a749efbc052
Nathalia Rus <nathaliarus@live.fr>
2019-11-15 16:37:46 +0000

    add random word generated by englishwords library

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final wordPair = WordPair.random();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,8 +13,8 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Welcome to Flutter"),
         ),
-        body: const Center(
-          child: const Text('Hello World'),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
